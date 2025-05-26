dd7e8aae0874aafcc24d28cd5ce05a8737335ccb
Mahdi Lotfian <80mahde@gmail.com>
2025-05-27 01:35:40 +0330

    Update base.html and urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home_page'),
-    path('browse/', BrowseView.as_view(), name='brows_page'),
+    path('browse/', BrowseView.as_view(), name='browse_page'),
     path('actors/', SearchActorsView.as_view(), name='actors_page'),
     path('api/ai-search/', AiDiscoveryView.as_view(), name='ai_discovery_page'),
 ]

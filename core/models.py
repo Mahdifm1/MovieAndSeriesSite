65da2ec48d4ace46592140d3736f8be84ca7ddd5
Mahdi Lotfian <80mahde@gmail.com>
2025-05-28 00:55:25 +0330

    update core models for saving user watchlist and likes into database

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class UserLike(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='likes')
+    item_id = models.IntegerField()
+    item_type = models.CharField(max_length=10, choices=[('movie', 'Movie'), ('tv', 'TV Series')])
+    item_title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'item_id', 'item_type')
+
+class UserWatchlist(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='watchlist')
+    item_id = models.IntegerField()
+    item_type = models.CharField(max_length=10, choices=[('movie', 'Movie'), ('tv', 'TV Series')])
+    item_title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'item_id', 'item_type')

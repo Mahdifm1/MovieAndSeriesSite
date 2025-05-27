55c8204af13dbbf517fc3e9b4f549b321961ae59
Mahdi Lotfian <80mahde@gmail.com>
2025-05-28 00:56:11 +0330

    add humanize to settings apps

--- MovieAndSeriesProject/settings.py
+++ MovieAndSeriesProject/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
     # celery
     'django_celery_results',
